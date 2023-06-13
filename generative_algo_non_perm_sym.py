a6b0f16381c6039f2aa9f3c27d8f24305dcb980e
PaulinDS <92310284+PaulinDS@users.noreply.github.com>
2023-06-13 14:56:07 +0200

    Update generative_algo_non_perm_sym.py

--- generative_algo_non_perm_sym.py
+++ generative_algo_non_perm_sym.py
@@ -1,6 +1,3 @@
-from forging_functions import *
-from generative_algo_functions import *
-
 import netket as nk
 import openfermion as of
 from openfermion import jordan_wigner
@@ -17,56 +14,40 @@ import json
 from functools import partial
 import optax
 from optax import adabelief, sgd
+import time
+
+import config
+
+from forging_functions import *
+from generative_algo_functions import *
 
 
 
 
-#########    Define the of Hamitonian    #########
+#########    The Hamitonian and the ARNN should be defined in the config file    #########
 
 
-#TFIM 1d 14 spins, rdm uni ext field
-n_qubits = 14
-key = random.PRNGKey(1234)
-key, subkey = random.split(key)
-h_i = random.uniform(subkey, (n_qubits,), dtype = jnp.float32).tolist()
-H_of = QubitOperator('Z{} Z0'.format(n_qubits-1))
-for i in range(n_qubits):
-  H_of += QubitOperator('Z{} Z{}'.format(i, i+1)) 
-  H_of += h_i[0]*QubitOperator('X{}'.format(i))
-  
-n_qubits = n_qubits
-N = n_qubits//2
-print("Number of qubits: ", n_qubits)
-print("Subsystems size: ", N)   
+print("Number of qubits: ", config.n_qubits)
+print("Subsystems size: ", config.N)   
+
+H = get_sparse_operator(config.H_of)
+E = get_ground_state(H)[0]
+print("GS energy:", E)
 
 
-
-#########    Convert the of Hamitonian    #########
-
-
-
-H_A, H_B, H_overlap_A, H_overlap_B, H_overlap_coef_jnp = creat_EF_hamiltonian(H_of, n_qubits)
+print('The classical NN defined is a:')
+print(config.model)
 
 
-
-#########    ARNN with netket     #########
-
-alpha = 2
-NN_features = alpha*2*N #dimension of the hidden neurons
-NN_layers = 5 #nbr of layers
-
-hi = nk.hilbert.Spin(s=0.5, N=int(2*N)) 
-sa = nk.sampler.ARDirectSampler(hi) 
-#sa = myARDirectSampler(hi) #if we want to control the number of one
-model = nk.models.ARNNDense(hilbert=hi, layers= NN_layers, features=NN_features, param_dtype = np.float32)
-get_sample = partial(sample_NN_non_perm_sym, sa = sa, NN_model = model, n_qubits = n_qubits)
-
-# Initialize it
+# Initialize the ARNN
 key = random.PRNGKey(123)
 key, subkey = random.split(key)
-s = jnp.ones(shape = (8, 2*N), dtype=jnp.int32)
+if config.perm_sym == True:
+  s = jnp.ones(shape = (8, config.N), dtype=jnp.int32)
+else:
+  s = jnp.ones(shape = (8, 2*config.N), dtype=jnp.int32)
 _, subkey = random.split(subkey)
-NN_params = model.init(subkey, s)
+NN_params = config.model.init(subkey, s)
 
 
 
@@ -89,7 +70,7 @@ def Loss_ARNN(NN_params, set_bitstring_syst, lambdas):
   set_bitstring_syst: set of bitstrings we want to train on
   lambdas: target Schmidt coefficient
   """
-  pi = jnp.exp(model.apply(NN_params, 2*(A_new-0.5)))
+  pi = jnp.exp(config.model.apply(NN_params, 2*(A_new-0.5)))
 
   ## MMD
   L = 0
@@ -111,23 +92,23 @@ val_grad_ARNN = jax.jit(jax.value_and_grad(Loss_ARNN, argnums = 0))
 #########    Simulation    #########
 
 
-A = jnp.array([[0, 1, 0, 0, 0, 0, 0],
-       [1, 0, 1, 0, 0, 1, 0],
-       [0, 0, 0, 0, 1, 1, 0],
-       [1, 1, 1, 1, 0, 1, 0],
-       [0, 0, 1, 1, 1, 0, 0],
-       [0, 0, 0, 1, 0, 1, 0],
-       [0, 1, 1, 0, 0, 0, 0],
-       [1, 1, 0, 0, 0, 1, 0]], dtype = np.int32)
-B = jnp.array([[0, 1, 0, 0, 1, 1, 0],
-       [0, 0, 0, 0, 0, 1, 0],
-       [0, 0, 0, 1, 0, 1, 0],
-       [1, 1, 0, 1, 1, 0, 0],
-       [1, 1, 1, 0, 0, 0, 0],
-       [0, 0, 1, 1, 0, 1, 0],
-       [1, 1, 1, 1, 1, 0, 0],
-       [0, 1, 0, 1, 1, 0, 0]], dtype = np.int32)
-
+# Initialize the sets of bitstrings
+A = jnp.array([[0, 1, 0, 0, 0, 0],
+       [1, 0, 1, 0, 0, 1],
+       [0, 0, 0, 0, 1, 1],
+       [1, 1, 1, 1, 0, 1],
+       [0, 0, 1, 1, 1, 0],
+       [0, 0, 0, 1, 0, 1],
+       [0, 1, 1, 0, 0, 0],
+       [1, 1, 0, 0, 0, 1]], dtype = np.int32)
+B = jnp.array([[0, 1, 0, 0, 1, 1],
+       [0, 0, 0, 0, 0, 1],
+       [0, 0, 0, 1, 0, 1],
+       [1, 1, 0, 1, 1, 0],
+       [1, 1, 1, 0, 0, 0],
+       [0, 0, 1, 1, 0, 1],
+       [1, 1, 1, 1, 1, 0],
+       [0, 1, 0, 1, 1, 0]], dtype = np.int32)
 
 
 
@@ -142,7 +123,7 @@ history_loss = []
 k = 8 #cutoff
 
 n_layers = 5 #number of layers in the variational circuits
-params_shape = (n_layers, N, 3)
+params_shape = (n_layers, config.N, 3)
 key = random.PRNGKey(1234)
 key, subkey = random.split(key)
 params_A = random.uniform(subkey, params_shape, dtype = np.float32) #parameters for the circuit acting on subsystem A
@@ -151,6 +132,13 @@ params_B = random.uniform(subkey, params_shape, dtype = np.float32) #paramters f
 Schmidt_coef = jnp.ones((k,), dtype = np.float32) #Schmidt coefficient
 Schmidt_coef = Schmidt_coef/jnp.sqrt(jnp.sum(Schmidt_coef**2))
 
+
+start = time.time()
+print("start jitting the generative loop")
+A_new, B_new, set_bitstring_syst_new, lambdas_new = Generative_loop_non_perm_sym(NN_params, params_A, params_B, A, B, subkey)
+end = time.time()
+jit_time = (end-start)/60
+print("Time to Jit: ", jit_time, " Minutes")
 
 
 print("We start with A0: ")
