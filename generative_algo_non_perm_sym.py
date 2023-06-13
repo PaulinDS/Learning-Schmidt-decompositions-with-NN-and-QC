db449b15324a171476a42978a99ec472cf4fdf7f
PaulinDS <92310284+PaulinDS@users.noreply.github.com>
2023-06-13 11:51:19 +0200

    Update generative_algo_non_perm_sym.py

--- generative_algo_non_perm_sym.py
+++ generative_algo_non_perm_sym.py
@@ -1,4 +1,5 @@
 from forging_functions import *
+from generative_algo_functions import *
 
 import netket as nk
 import openfermion as of
@@ -58,7 +59,7 @@ hi = nk.hilbert.Spin(s=0.5, N=int(2*N))
 sa = nk.sampler.ARDirectSampler(hi) 
 #sa = myARDirectSampler(hi) #if we want to control the number of one
 model = nk.models.ARNNDense(hilbert=hi, layers= NN_layers, features=NN_features, param_dtype = np.float32)
-get_sample = partial(sample_NN, sa = sa, NN_model = model, n_qubits = n_qubits)
+get_sample = partial(sample_NN_non_perm_sym, sa = sa, NN_model = model, n_qubits = n_qubits)
 
 # Initialize it
 key = random.PRNGKey(123)
@@ -68,7 +69,7 @@ _, subkey = random.split(subkey)
 NN_params = model.init(subkey, s)
 
 
-from generative_algo_functions import *
+
 
 
 
